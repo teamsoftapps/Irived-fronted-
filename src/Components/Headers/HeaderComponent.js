2d513dff8f2d2461e8bb4c8f4f768ea5c8eb5a13
Development-loop <66966682+teamsoftapps@users.noreply.github.com>
2024-05-09 07:49:34 -0700

    7:50

--- src/Components/Headers/HeaderComponent.js
+++ src/Components/Headers/HeaderComponent.js
@@ -7,26 +7,26 @@ import {
 } from 'react-native-responsive-dimensions';
 import {images} from '../../utils';
 
-const HeaderComponent = () => {
+const HeaderComponent = ({text = ''}) => {
   return (
     <View style={styles.topbar}>
-      <TouchableOpacity>
+      <TouchableOpacity style={{}}>
         <Image
           source={images.back}
           style={{width: responsiveWidth(6), height: responsiveHeight(2.5)}}
         />
       </TouchableOpacity>
-      <Text
-        style={{
-          fontSize: responsiveFontSize(2.5),
-          color: 'black',
-          fontWeight: '600',
-          textAlign: 'center',
-          flex: 1,
-          backgroundColor: 'red',
-        }}>
-        Notification
-      </Text>
+      <View style={{flex: 1}}>
+        <Text
+          style={{
+            fontSize: responsiveFontSize(2.5),
+            color: 'black',
+            fontWeight: '600',
+            textAlign: 'center',
+          }}>
+          Cart
+        </Text>
+      </View>
     </View>
   );
 };
@@ -35,6 +35,7 @@ export default HeaderComponent;
 
 const styles = StyleSheet.create({
   topbar: {
+    height: responsiveHeight(10),
     paddingHorizontal: responsiveWidth(8),
     flexDirection: 'row',
     alignItems: 'center',
