ab75bebc89ca85427d32fc964c67588037cb4f6c
Development-loop <66966682+teamsoftapps@users.noreply.github.com>
2024-05-09 15:02:12 -0700

    8gjh

--- src/Screens/Message.js
+++ src/Screens/Message.js
@@ -1,10 +1,19 @@
-import {StyleSheet, Text, View, SafeAreaView, ScrollView} from 'react-native';
+import {
+  StyleSheet,
+  Image,
+  Text,
+  View,
+  SafeAreaView,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
 import {
   responsiveFontSize,
   responsiveHeight,
   responsiveWidth,
 } from 'react-native-responsive-dimensions';
 import React from 'react';
+import {images} from '../utils';
 
 const Message = () => {
   return (
@@ -12,9 +21,48 @@ const Message = () => {
       <ScrollView showsVerticalScrollIndicator={false} style={{flex: 1}}>
         <View
           style={{
-            height: responsiveHeight(8),
+            height: responsiveHeight(10),
+            paddingHorizontal: responsiveWidth(8),
+            flexDirection: 'row',
+            alignItems: 'center',
             backgroundColor: 'white',
-          }}></View>
+          }}>
+          <TouchableOpacity style={{}}>
+            <Image
+              source={images.back}
+              style={{
+                width: responsiveWidth(6),
+                height: responsiveHeight(2.8),
+                resizeMode: 'contain',
+              }}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity style={{}}>
+            <Image
+              source={images.profile}
+              style={{
+                width: responsiveWidth(12),
+                height: responsiveWidth(12),
+                borderRadius: responsiveWidth(10),
+                objectFit: 'cover',
+                marginHorizontal: responsiveWidth(3),
+              }}
+            />
+          </TouchableOpacity>
+          <View>
+            <Text>Martin Mork</Text>
+            <View style={{flexDirection:"row", alignItems:"center", gap:responsiveWidth(1)}}>
+              <View
+                style={{
+                  backgroundColor: '#4361EE',
+                  width: responsiveWidth(2),
+                  height: responsiveWidth(2),
+                  borderRadius: responsiveWidth(4),
+                }}></View>
+              <Text style={{color:"#4361EE"}}>Online</Text>
+            </View>
+          </View>
+        </View>
         <View style={styles.container}>
           <Text style={styles.text}>Today</Text>
         </View>
