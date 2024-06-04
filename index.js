b6275dd2f36bf3826c1dbaf06a55cd69531223d5
teamsoftapps <softapps.io3@gmail.com>
2024-06-04 06:06:29 +0500

    shahzaib

--- index.js
+++ index.js
@@ -7,12 +7,7 @@ import App from './App';
 import {name as appName} from './app.json';
 import {Provider} from 'react-redux';
 import {Store, persistor} from './src/Store/store';
-import Favourites from './src/Screens/Favourites';
 import Filter from './src/Screens/Filter';
-import PersonalInfo from './src/Screens/PersonalInfo';
-import Extra from './src/Screens/Extra';
-import Order from './src/Screens/Orders';
-import Orders from './src/Screens/Review&Rating';
 
 const Routes = () => {
   return (
@@ -22,4 +17,4 @@ const Routes = () => {
   );
 };
 
-AppRegistry.registerComponent(appName, () => Order);
+AppRegistry.registerComponent(appName, () => App);
