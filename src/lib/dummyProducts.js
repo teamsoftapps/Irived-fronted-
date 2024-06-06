e79b600a3f968307b9649c69709e9bf71594b34b
teamsoftapps <softapps.io3@gmail.com>
2024-06-07 02:56:21 +0500

    2:56

---
+++ src/lib/dummyProducts.js
@@ -0,0 +1,45 @@
+export const products = {
+  products: [
+    {
+      id: '1',
+      createdAt: '2024-06-07T12:00:00Z',
+      title: 'Product A',
+      desc: 'Description for Product A',
+      img: 'product_a.jpg',
+      price: 49.99,
+      isFeatured: true,
+      options: [
+        {size: 'Small', color: 'Red'},
+        {size: 'Large', color: 'Blue'},
+      ],
+      category: {
+        id: '1',
+        createdAt: '2024-06-07T12:00:00Z',
+        title: 'Category A',
+        desc: 'Description for Category A',
+        color: '#FF5733',
+        img: 'category_a.jpg',
+        slug: 'category-a',
+      },
+      catSlug: 'category-a',
+    },
+    {
+      id: '2',
+      createdAt: '2024-06-07T12:00:00Z',
+      title: 'Product B',
+      desc: 'Description for Product B',
+      price: 29.99,
+      options: [{size: 'Medium', color: 'Green'}],
+      category: {
+        id: '2',
+        createdAt: '2024-06-07T12:00:00Z',
+        title: 'Category B',
+        desc: 'Description for Category B',
+        color: '#3498db',
+        img: 'category_b.jpg',
+        slug: 'category-b',
+      },
+      catSlug: 'category-b',
+    },
+  ],
+};
